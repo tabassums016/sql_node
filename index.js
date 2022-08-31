5a75b50c0c8ffad7f4b2b21c49223c29568e2fb5
tabassums016 <tabassums016@gmail.com>
2022-08-31 23:17:15 +0530

    mqtt connection added

--- index.js
+++ index.js
@@ -1,9 +1,25 @@
 const sql= require('mysql');
+var mqtt=require('mqtt');
+
+options={
+    clientId:"mqttjs01",
+    username:"",
+    password:"",
+    clean:true};
+
+var client = mqtt.connect("mqtt://test.mosquitto.org:1883",options)
+const data_topic= 'test/data';
+
+client.on('connect', () => {
+  console.log("connected to mqtt broker")
+  client.subscribe(data_topic)
+  // client.subscribe(logs_topic)
+})
 
 const db= sql.createConnection({
     host:'localhost',
     user:'root',
-    password:'TEST123',
+    password:'tabz',
     database:'nodesql',
     
 });
@@ -14,27 +30,36 @@ db.connect((err)=>{
     }
     console.log("sql connected");
 });
-let usetable='USE nodesql';
-let createtable='CREATE TABLE Mydata (Device int,TS int,DATA varchar(200));';
+let usedb='USE nodesql';
+let createtable='CREATE TABLE mqttdata (Device varchar(50),TS int,DATA varchar(200), PRIMARY KEY (TS));';
 
-db.query(usetable, (err,result)=>{
+
+db.query(usedb, (err,result)=>{
     if(err)throw err;
     console.log(result);
 });
-db.query(createtable, (err,result)=>{
-    if(err)throw err;
-    console.log(result);
-});
+// db.query(createtable, (err,result)=>{
+//     if(err)throw err;
+    // console.log(result);
+// });
 
-let update= "INSERT INTO Mydata(Device, TS, DATA) VALUES( 1, 1638, 'HELLO WORLD')";
+client.on('message', (topic, message) => {
+  
+    console.log('DATA: %s', message)
+    mqtt_json=JSON.parse(message)
+    console.log('RESPONSE: %s %d %s', mqtt_json.device, mqtt_json.ts, mqtt_json.data[0])
+    let update= `INSERT INTO mqttdata(Device, TS, DATA) VALUES( "${mqtt_json.device}", ${mqtt_json.ts}, "${mqtt_json.data[0]}")`;
+   console.log(update);
 db.query(update, (err,result)=>{
     if(err)throw err;
     console.log(result);
 });
+  })
+  
 
-let show='SELECT * FROM Mydata';
-db.query(show, (err,result)=>{
-    if(err)throw err;
-    console.log(result);
-});
+// let show='SELECT * FROM Mydata';
+// db.query(show, (err,result)=>{
+//     if(err)throw err;
+//     console.log(result);
+// });
 
